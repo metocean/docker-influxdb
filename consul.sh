4838f78179620592dcab0801e57d93b65f901a55
Rafael Soutelino <r.soutelino@metocean.co.nz>
2017-02-08 15:46:07 +1300

    making consul simples

--- consul.sh
+++ consul.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-if [ -z "$CONSULDATA" ]; then export CONSULDATA="/tmp/consul-data";fi
-if [ -z "$CONSULDIR" ]; then export CONSULDIR="/consul";fi
-if [ "$(ls -A $CONSULDIR)" ]; then
-    exec consul agent -data-dir=$CONSULDATA -config-dir=$CONSULDIR &
-fi
-PID=$!
-wait $PID
-trap - TERM INT
-wait $PID
+exec consul agent -data-dir=/tmp/consul-data -config-dir=/consul &
+
